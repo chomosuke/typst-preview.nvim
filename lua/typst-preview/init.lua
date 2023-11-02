594bc47d5a9bce1ffad2b129ad269981ebd4c98b
chomosuke <a13323600@gmail.com>
2023-11-02 12:26:08 +1100

    Some plans

---
+++ lua/typst-preview/init.lua
@@ -0,0 +1,5 @@
+-- Parts:
+-- Download typst-preview binary from github, put it in vim.fn.stdpath "data".
+-- Launch typst-preview server, and connect to it.
+-- Implement all events.
+-- Remember to kill the server so there are no process leak.
