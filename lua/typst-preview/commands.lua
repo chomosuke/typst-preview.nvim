605a66f2f29f29c50a636ba4151a8005fd875c59
chomosuke <a13323600@gmail.com>
2023-11-04 12:49:21 +1100

    TypstPreviewUpdate and TypstPreviewToggle

--- lua/typst-preview/commands.lua
+++ lua/typst-preview/commands.lua
@@ -1,15 +1,33 @@
 local events = require 'typst-preview.events'
+local fetch = require 'typst-preview.fetch'
 
 local M = {}
 
--- TODO: TypstPreviewToggle, TypstPreviewUpdate
+local previewing = {}
+
 function M.create_commands()
-  vim.api.nvim_create_user_command('TypstPreview', function()
-    events.watch(vim.fn.bufnr())
+  local function preview_on()
+    if not previewing[vim.fn.bufnr()] then
+      previewing[vim.fn.bufnr()] = true
+      events.watch(vim.fn.bufnr())
+    end
+  end
+  local function preview_off()
+    if previewing[vim.fn.bufnr()] then
+      previewing[vim.fn.bufnr()] = false
+      events.stop(vim.fn.bufnr())
+    end
+  end
+  vim.api.nvim_create_user_command('TypstPreview', preview_on, {})
+  vim.api.nvim_create_user_command('TypstPreviewStop', preview_off, {})
+  vim.api.nvim_create_user_command('TypstPreviewToggle', function()
+    if previewing[vim.fn.bufnr()] then
+      preview_off()
+    else
+      preview_on()
+    end
   end, {})
-  vim.api.nvim_create_user_command('TypstPreviewStop', function()
-    events.stop(vim.fn.bufnr())
-  end, {})
+  vim.api.nvim_create_user_command('TypstPreviewUpdate', fetch.fetch, {})
 end
 
 return M
