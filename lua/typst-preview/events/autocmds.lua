64a3949389f16107974c70fccd7cd46e3db49fb1
chomosuke <a13323600@gmail.com>
2023-11-27 22:03:02 +1100

    set_follow_cursor, get_follow_cursor, sync_with_cursor

--- lua/typst-preview/events/autocmds.lua
+++ lua/typst-preview/events/autocmds.lua
@@ -5,6 +5,39 @@ local communicator = require 'typst-preview.events.communicator'
 
 local M = {}
 
+-- TODO per buffer panel_scroll config
+local panel_scroll = true
+
+---Set the way preview scrolling respond to cursor movement
+---@param enabled boolean
+function M.set_follow_cursor(enabled)
+  panel_scroll = enabled
+end
+
+---Get current preview scrolling mode
+---@return boolean
+function M.get_follow_cursor()
+  return panel_scroll
+end
+
+local function sync_with_cursor(comm, bufnr)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local line = cursor[1] - 1
+  comm.write(vim.fn.json_encode {
+    event = 'panelScrollTo',
+    filepath = server.get_buffer_path(bufnr),
+    line = line,
+    character = cursor[2],
+  } .. '\n')
+end
+
+---Scroll preview to where cursor is regardless of preview scrolling mode
+function M.sync_with_cursor()
+  for _, comm in pairs(communicator.comms) do
+    sync_with_cursor(comm, vim.fn.bufnr())
+  end
+end
+
 ---register autocmds for this buffer to do the same for all comms
 local function register_autocmds(bufnr)
   local last_line
@@ -24,7 +57,7 @@ local function register_autocmds(bufnr)
     {
       event = { 'CursorMoved', 'CursorMovedI' },
       callback = function(comm, ev)
-        if super.suppress_on_scroll then
+        if super.suppress_on_scroll or not panel_scroll then
           return
         end
         utils.debug('scrolling: ' .. bufnr)
@@ -33,12 +66,7 @@ local function register_autocmds(bufnr)
         if last_line ~= line or ev.event ~= 'CursorMovedI' then
           -- No scroll when on the same line in insert mode
           last_line = line
-          comm.write(vim.fn.json_encode {
-            event = 'panelScrollTo',
-            filepath = server.get_buffer_path(bufnr),
-            line = line,
-            character = cursor[2],
-          } .. '\n')
+          sync_with_cursor(comm, bufnr)
         end
       end,
     },
--- lua/typst-preview/init.lua
+++ lua/typst-preview/init.lua
@@ -1,7 +1,11 @@
-local config = require "typst-preview.config"
+local config = require 'typst-preview.config'
+local autocmds = require 'typst-preview.events.autocmds'
 
 local M = {
   setup = config.config,
+  set_follow_cursor = autocmds.set_follow_cursor,
+  get_follow_cursor = autocmds.get_follow_cursor,
+  sync_with_cursor = autocmds.sync_with_cursor,
 }
 
 return M
