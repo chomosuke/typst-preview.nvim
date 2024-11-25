051d5b41e7de91e3a2004f14b2526988a33eb2c2
chomosuke <a13323600@gmail.com>
2024-11-25 16:15:34 +1100

    bumped tinymist version

--- lua/typst-preview/fetch.lua
+++ lua/typst-preview/fetch.lua
@@ -180,7 +180,7 @@ end
 function M.bins_to_fetch()
   return {
     {
-      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.0/'
+      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.4/'
         .. M.get_tinymist_bin_name(),
       bin_name = M.get_tinymist_bin_name(),
       name = 'tinymist',
