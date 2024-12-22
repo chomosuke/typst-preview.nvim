1c2c005ee3f5012d496956feba1e6fb93f7891c3
chomosuke <a13323600@gmail.com>
2024-12-22 14:43:10 +1100

    tinymist v0.12.12

--- lua/typst-preview/fetch.lua
+++ lua/typst-preview/fetch.lua
@@ -180,7 +180,7 @@ end
 function M.bins_to_fetch()
   return {
     {
-      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.10/'
+      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.12.12/'
         .. M.get_tinymist_bin_name(),
       bin_name = M.get_tinymist_bin_name(),
       name = 'tinymist',
