25aa102eafcafa405b98f95d2816fd48e6614725
Patrick Massot <patrickmassot@free.fr>
2026-06-19 15:34:44 +0200

    Update tinymist version

--- lua/typst-preview/fetch.lua
+++ lua/typst-preview/fetch.lua
@@ -180,7 +180,7 @@ end
 function M.bins_to_fetch()
   return {
     {
-      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.14.12/'
+      url = 'https://github.com/Myriad-Dreamin/tinymist/releases/download/v0.15.0/'
           .. M.get_tinymist_bin_name(),
       bin_name = M.get_tinymist_bin_name(),
       name = 'tinymist',
